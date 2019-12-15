d2b37ae99168196982c3895890af2e19f4605b3e
Michael Ryan <michael_ryan@Michaels-Air.fios-router.home>
2019-12-14 22:22:00 -0500

    Added walkthrough code to Teacher.java.

--- src/com/apcsa/model/Teacher.java
+++ src/com/apcsa/model/Teacher.java
@@ -1,6 +1,8 @@
 package com.apcsa.model;
 
 import com.apcsa.model.User;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Teacher extends User {
 
@@ -10,4 +12,22 @@ public class Teacher extends User {
     private String lastName;
     private String departmentName;
 
+    /**
+     * Creates an instance of the Teacher class.
+     * 
+     * @param user
+     * @param rs 
+     * @throws SQLException
+     */
+    
+    public Teacher(User user, ResultSet rs) throws SQLException {
+    	super(user);
+    	
+    	this.teacherId = rs.getInt("teacher_id");
+    	this.departmentId = rs.getInt("department_id");
+    	this.firstName = rs.getString ("first_name");
+    	this.lastName = rs.getString ("last_name");
+    	this.departmentName = rs.getString("department_id");
+    }
+    
 }
