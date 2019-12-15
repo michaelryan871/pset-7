44cf268f18ba81d5cc3d83d55f52e542f99ad67f
Michael Ryan <michael_ryan@Michaels-Air.fios-router.home>
2019-12-14 23:00:29 -0500

    Added walkthrough code to Student.java.

--- src/com/apcsa/model/Student.java
+++ src/com/apcsa/model/Student.java
@@ -1,6 +1,8 @@
 package com.apcsa.model;
 
 import com.apcsa.model.User;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Student extends User {
 
@@ -11,5 +13,26 @@ public class Student extends User {
     private double gpa;
     private String firstName;
     private String lastName;
+    private String fullName = firstName + lastName;
+    
+    /**
+     * Creates an instance of the Student class.
+     * 
+     * @param user
+     * @param rs
+     * @throws SQLException
+     */
+    
+    public Student(User user, ResultSet rs) throws SQLException {
+    	super(user);
+    	
+    	this.studentId = rs.getInt("student_id");
+    	this.classRank = rs.getInt("class_rank");
+    	this.gradeLevel = rs.getInt("grade_level");
+    	this.graduationYear = rs.getInt("graduation");
+    	this.gpa = rs.getDouble("gpa");
+    	this.firstName = rs.getString("first_name");
+    	this.lastName = rs.getString("last_name");
+    }
     
 }
