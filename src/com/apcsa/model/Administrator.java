33fb5b0a5ebe0c7b1226fab2743a482127225388
Michael Ryan <michael_ryan@Michaels-Air.fios-router.home>
2019-12-14 21:29:33 -0500

    Added walkthrough code to Administrator.java.

--- src/com/apcsa/model/Administrator.java
+++ src/com/apcsa/model/Administrator.java
@@ -9,4 +9,22 @@ public class Administrator extends User {
     private String lastName;
     private String jobTitle;
 
+    /** 
+     * Creates an instance of the Administrator class. 
+     * 
+     * @param user
+     * @param rs 
+     * @throws SQLException
+     */
+    
+    public Administrator(User user, ResultSet rs) throws SQLException {
+    	super(user);
+    	
+    	this.administratorId = rs.getInt("administrator_id");
+    	this.firstName = rs.getString("first_name");
+    	this.lastName = rs.getString("last_name");
+    	this.jobTitle = rs.getString("job_title");
+    	
+    }
+    
 }
